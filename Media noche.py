db0e3c776b18bdad298732dceb88c12a598fecd3
AmanecidosDesdeAyer <85531748+AmanecidosDesdeAyer@users.noreply.github.com>
2021-06-19 15:19:52 -0500

    Add files via upload

---
+++ Media noche.py
@@ -0,0 +1,23 @@
+#--------------Menu-----------------------------
+
+menuPrincipal = int(input("MENU DE CAFE: \n1)Café con leche       s/8.50 \n2)Café largo           s/7.00 \n3)Café solo            s/5.50 \n4)Café americano       s/10.00 \n5)Café doble           s/12.00 \n6)Salir \nELIJA UNA OPCION: "))
+while menuPrincipal !=0:
+
+    if menuPrincipal ==1:
+        print("Cafe con leche s/8.50")
+    elif menuPrincipal ==2:
+        print("Cafe largo s/7.00")
+    elif menuPrincipal ==3:
+        print("Cafe solo s/5.50")
+    elif menuPrincipal ==4:
+        print("Cafe americano s/10.00")
+    elif menuPrincipal ==5:
+        print("Cafe doble s/12.00")
+    elif menuPrincipal ==6:
+        print("Salir")
+    else:
+        print("Por favor digita una opcion correcta")
+
+    
+
+
